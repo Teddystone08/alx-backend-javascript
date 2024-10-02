c257e5259bd94d1934047594bbe9d835b29869bc
Teddystone08 <ernestlrty@hotmail.com>
2024-10-02 02:21:01 +0000

    updated

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulate an asynchronous operation
-        setTimeout(() => {
-            resolve('Response from API');
-        }, 1000);
-    });
-}
+export function getResponseFromAPI() {
+    return new Promise(() => ());
+} 
