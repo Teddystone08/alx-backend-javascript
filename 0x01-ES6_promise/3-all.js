d938400db98208b44e066e5da0a8d811ee2dc269
Teddystone08 <ernestlrty@hotmail.com>
2024-10-03 04:34:06 +0000

    import

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+    // Use Promise.all to resolve both promises
+    Promise.all([uploadPhoto(), createUser()])
+        .then(([photo, user]) => {
+            // Access the resolved values and log them
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        })
+        .catch(() => {
+            // Handle any error and log the fallback message
+            console.log('Signup system offline');
+        });
+}
