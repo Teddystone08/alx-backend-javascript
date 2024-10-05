65692d6cef24ce44a92776b3e28f166393e1cc27
Teddystone08 <ernestlrty@hotmail.com>
2024-10-05 04:48:25 +0000

    added function

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,14 @@
+/**
+ * function taskFirst
+ * @returns {string} task
+ */
+
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+}
+
+export function taskNext() {
+    let task = 'But sometimes let is okay';
+    return task;
+}
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,5 @@
+# 0x00. ES6 Basics
+
+## Setup
+- Install NodeJS 12.11.x (in your directory)
+- Install Jest, Babel, and ESLint
