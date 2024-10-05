ee8f87348f85e1f3b835052d3ca539ef6579f893
Teddystone08 <ernestlrty@hotmail.com>
2024-10-05 01:06:13 +0000

    linked class

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,8 @@
 import Classroom from "./0-classroom.js";
+/**
+ * create an array class of @linked Classroom with size
+ * @return: anarray of @linked Classroom
+ */
 
 export default function initializeRooms() {
     const rooms = [
