a811f978a12400dbbb64ca77e4f31f7b10fae333
Teddystone08 <ernestlrty@hotmail.com>
2024-10-05 00:59:49 +0000

    amendclass

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,13 @@
-export default class Classroom {
-    constructor(maxStudentsSize) {
-        this.maxStudentsSize = maxStudentsSize;
-    }
-   
+/**
+ * Represents a class room.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
