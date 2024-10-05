a5445449e3aea5cc96a084483e3171e830e1163b
Teddystone08 <ernestlrty@hotmail.com>
2024-10-05 04:55:53 +0000

    boolean returen

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,17 @@
+/**
+ * construct a function that takes a boolean as an argument and returns 
+ * an array of two booleans. The first boolean should be false and the
+ * @param {task, task2} trueOrFalse 
+ * @returns boolean
+ */
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+
+    if (trueOrFalse) {
+        const task = true;
+        const task2 = false;
+    }
+
+    return [task, task2];
+}
