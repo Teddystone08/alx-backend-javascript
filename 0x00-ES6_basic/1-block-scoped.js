4f6236b0a6c05340eb614a8693ddcd6ff0bf52b0
Teddystone08 <ernestlrty@hotmail.com>
2024-10-06 16:20:00 +0000

    const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,18 +1,11 @@
-/**
- * construct a function that takes a boolean as an argument and returns
- * an array of two booleans. The first boolean should be false and the
- * @param const, let true Or False
- * @returns boolean
- */
-export default function taskBlock(trueOrFalse){
-	const task = false;
-	const task2 = true;
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
 
-	if (trueOrFalse){
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false; 
+  }
 
-		const task = true;
-		const task2 = false;
-	}
-
-    return [task, task2];
+  return [task, task2];
 }
