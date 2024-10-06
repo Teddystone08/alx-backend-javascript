fc615229a783e338d3c9d52b361ad9f1000aefd2
Teddystone08 <ernestlrty@hotmail.com>
2024-10-06 03:17:33 +0000

    revert

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,12 +4,6 @@ import Classroom from "./0-classroom.js";
  * @return: anarray of @linked Classroom
  */
 
-import ClassRoom from './0-classroom.js';
-
 export default function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+    return [19, 20, 34].map((size) => new Classroom(size));
 }
