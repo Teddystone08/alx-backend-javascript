47ec115daefb8a58b44bfeaae5bdaceb8f9edca0
Teddystone08 <ernestlrty@hotmail.com>
2024-10-09 21:22:22 +0000

    disable type

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,6 +1,7 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -10,5 +10,6 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
     [`capita-${getCurrentYear()}`]: capita,
   };
 
+  
   return budget;
 }
