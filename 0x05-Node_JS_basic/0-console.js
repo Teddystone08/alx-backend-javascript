5643194fbd3017d993edbc7d39994cf30cb77d05
Teddystone08 <ernestlrty@hotmail.com>
2024-11-28 05:38:37 +0000

    all

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (string) => console.log(string);
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
+module.exports = displayMessage
