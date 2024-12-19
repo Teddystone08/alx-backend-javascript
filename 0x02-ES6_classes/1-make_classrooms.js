d0715f8684d2dcf6fa4de32e89f8ce02d5ab22b0
Teddystone08 <ernestlrty@hotmail.com>
2024-12-19 21:06:32 +0000

    size

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,5 @@
-import Classroom from "./0-classroom.js";
-/**
- * create an array class of @linked Classroom with size
- * @return: anarray of @linked Classroom
- */
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-	const Csizes = [9, 20, 34];
-	return Csizes.map(size => new ClassRoom(size));
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
