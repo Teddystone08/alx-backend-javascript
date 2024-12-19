c6fd4c8905bd3c85edc9a9925905c45923be41b8
Teddystone08 <ernestlrty@hotmail.com>
2024-12-19 22:31:37 +0000

    price

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,44 +1,26 @@
-/**
-    * 
-    * @param {string} code - code of the currency
-    * @param {string} name - name of the currency
-    */
-
 export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
 
-    constructor(code, name) {
-        this.code = code;
-        this.name = name;
-    }
+  get code() {
+    return this._code;
+  }
 
-    // get code of the currency
-    get code() {
-        return this._code;
-    }
+  set code(code) {
+    this._code = code;
+  }
 
-    // set code of the currency
-    set code(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError('Code must be a string');
-        }
-        this._code = value;
-    }
+  get name() {
+    return this._name;
+  }
 
-    // get name of the currency
-    get name() {
-        return this._name;
-    }
+  set name(name) {
+    this._name = name;
+  }
 
-    // set name of the currency
-    set name(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._name = value;
-    }
-
-    // display the currency
-    displayFullCurrency() {
-        return `${this.name} (${this.code})`;
-    }
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
